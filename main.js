1f755edebaede784908e785baf89d2ddbb2ea36d
DIKIIDM <4dmitrich@gmail.com>
2021-04-09 14:20:03 +0300

    add nedb database

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
 const express = require('express')
      ,swig = require('swig')
      ,riot = require('riot')
-     ,tagClientList = require('./app/tags/client_list.tag');
+     ,tagClientList = require('./app/tags/client_list.tag')
+	 ,Datastore = require('nedb');
 	 
 const app = new express();
+const dbUsers = new Datastore({filename: './app/db/nedb/users'});
 
-app.engine('html',swig.renderFile);
+app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views',__dirname + '/app/views');
 app.use(express.static(__dirname + '/app/public'))
 
 app.get('/',function(req,res) {	
   let data = {
-	  "clients": [
-		  { name: "Zhena"
-			,age: 33
-		  }
-		  ,{ name: "Denis"
-			,age: 31
-		  }
-		  ,{ name: "Ksu"
-			,age: 33
-		  }
-	  ]
+	  "clients": []
   }
-  let content = riot.render(tagClientList, data);
-  res.render('index', {tagContent: content});
+  dbUsers.loadDatabase();
+  dbUsers.find({}, function (err, dbRes) {
+	data.clients = dbRes;
+	let content = riot.render(tagClientList, data);
+	res.render('index', {tagContent: content});
+  });
 })
+
 app.listen(3000, function(){
   console.log('server listening on port 3000');
 })
+
