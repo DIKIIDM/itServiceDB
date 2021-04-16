d4834f937af5bb8d0140677f13e0809019f44743
DIKIIDM <4dmitrich@gmail.com>
2021-04-16 13:54:40 +0300

    add rest api

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const express = require('express')
-     ,swig = require('swig')
-     ,riot = require('riot')
-     ,tagClientList = require('./app/public/tags/client_list.tag')
-     ,tagClientCard = require('./app/public/tags/client_card.tag')
-	 ,Datastore = require('nedb');
-	 
+    ,bodyParser = require('body-parser')
+    ,swig = require('swig')
+    ,riot = require('riot')
+    ,tagClientList = require('./app/public/tags/client_list.tag')
+    ,tagClientCard = require('./app/public/tags/client_card.tag')
+    ,Datastore = require('nedb');
+
 const app = new express();
 const dbUsers = new Datastore({filename: './app/db/nedb/users'});
 
@@ -12,18 +13,21 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views',__dirname + '/app/views');
 app.use(express.static(__dirname + '/app/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //-------------------------------------------------------------
-app.get('/',function(req,res) {	
-  let data = {
-	  "clients": []
-  }
-  dbUsers.loadDatabase();
-  dbUsers.find({}, function (err, dbRes) {
-	data.clients = dbRes;
-	let content = riot.render(tagClientList, data);
-	res.render('index', {tagContent: content});
-  });
+// pages
+//-------------------------------------------------------------
+app.get('/',function(req,res) {
+    let data = {
+        "clients": []
+    }
+    dbUsers.loadDatabase();
+    dbUsers.find({}, function (err, dbRes) {
+        data.clients = dbRes;
+        let content = riot.render(tagClientList, data);
+        res.render('index', {tagContent: content});
+    });
 })
 //-------------------------------------------------------------
 app.get('/add_user',function(req,res) {
@@ -34,7 +38,23 @@ app.get('/add_user',function(req,res) {
     res.render('index', {tagContent: content});
 })
 
-app.listen(3000, function(){
-  console.log('server listening on port 3000');
-})
+//-------------------------------------------------------------
+// REST API
+//-------------------------------------------------------------
+app.post('/users',function(req,res) {
+    let newClient = {};
 
+    newClient.first_name = req.body.first_name;
+    newClient.middle_name = req.body.middle_name;
+    newClient.last_name = req.body.last_name;
+    newClient.phone = req.body.phone;
+
+    dbUsers.insert(newClient, function (err, dbRes) {
+
+    });
+    return res.send('POST HTTP method on user resource');
+});
+
+app.listen(3000, function(){
+    console.log('server listening on port 3000');
+})
